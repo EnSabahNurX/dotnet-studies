2cccf0564968c0c051c5a35999e7ffc9f16034f9
ricardo <ricklimadealmeida@gmail.com>
2021-04-08 18:43:21 -0300

    running the first cs

--- Structure/Program.cs
+++ Structure/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 
-namespace Structure
+namespace DigitalInnovationOne
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var s = new Pilha();
-            s.Empilha(1);
-            s.Empilha(10);
-            s.Empilha(100);
-            Console.WriteLine(s.Desempilha());
-            Console.WriteLine(s.Desempilha());
-            Console.WriteLine(s.Desempilha());
+            // Console.WriteLine("Hello World!");
+            int times = 5;
+            for (int i = 0; i < times; i++)
+            {
+                // Console.WriteLine($"Welcome to the .NET course {i}");
+                Console.WriteLine($"Welcome to the .NET course {i}");
+            }
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...\n");
+            Console.ReadKey(true);
         }
     }
 }
