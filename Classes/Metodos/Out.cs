0765053d4fa0e21678ec824be61cd971fc03b009
ricardo <ricklimadealmeida@gmail.com>
2021-04-10 20:11:57 -0300

    testing ref and out

--- Classes/Metodos/Out.cs
+++ Classes/Metodos/Out.cs
@@ -2,15 +2,15 @@ namespace Classes.Metodos
 {
     public class Out
     {
-        static void Dividir(int x, int y, out int resultado, out int resto) 
+        static void Dividir(int x, int y, out int resultado, out int resto)
         {
             resultado = x / y;
             resto = x % y;
         }
-        
-        public static void Dividir() 
+
+        public static void Dividir(int i, int j)
         {
-            Dividir(10, 3, out int resultado, out int resto);
+            Dividir(i, j, out int resultado, out int resto);
             System.Console.WriteLine("{0} {1}", resultado, resto);	// Escreve "3 1"
         }
     }
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Classes.Herança;
+using Classes.Metodos;
 
 namespace Classes
 {
@@ -11,6 +12,7 @@ namespace Classes
             // Ponto p1 = new Ponto(10, 20);
             // Ponto3D p2 = new Ponto3D(10, 20, 30);
             // Ponto3D.Calcular();
+            Out.Dividir(10, 3);
         }
     }
 }
