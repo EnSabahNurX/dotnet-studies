d875b8b58edf6ec6137aa06149e2f96731d42202
ricardo <ricklimadealmeida@gmail.com>
2021-04-11 06:45:57 -0300

    listing all students and calculating the average score

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -7,7 +7,7 @@ namespace Revisao
         static void Main(string[] args)
         {
             Student[] students = new Student[5];
-
+            var studentIndex = 0;
             string userOption = GetUserOption();
 
             while (userOption.ToUpper() != "X")
@@ -19,14 +19,40 @@ namespace Revisao
                         Student student = new Student();
                         student.Name = Console.ReadLine();
                         Console.WriteLine("Inform the student score:");
-                        student.Score = decimal.Parse(Console.ReadLine());
-
+                        if (decimal.TryParse(Console.ReadLine(), out decimal score))
+                        {
+                            student.Score = score;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("You must type a decimal value to score!");
+                        }
+                        students[studentIndex] = student;
+                        studentIndex++;
                         break;
                     case "2":
-
+                        foreach (var a in students)
+                        {
+                            if (!string.IsNullOrEmpty(a.Name))
+                            {
+                                Console.WriteLine($"Student: {a.Name}\nScore: {a.Score}");
+                            }
+                        }
                         break;
                     case "3":
+                        decimal total = 0;
+                        var studentsAmount = 0;
+                        for (int i = 0; i < students.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(students[i].Name))
+                            {
+                                total += students[i].Score;
+                                studentsAmount++;
+                            }
 
+                        }
+                        var average = total / studentsAmount;
+                        Console.WriteLine($"Total average: {average}");
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
@@ -37,6 +63,7 @@ namespace Revisao
 
         private static string GetUserOption()
         {
+            Console.WriteLine();
             Console.WriteLine("Please, select a option bellow:");
             Console.WriteLine("1 - Insert a new student");
             Console.WriteLine("2 - List all students");
