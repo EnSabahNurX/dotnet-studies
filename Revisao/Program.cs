d8b288544130a0a6a64f026274b5b419205da906
ricardo <ricklimadealmeida@gmail.com>
2021-04-11 05:29:20 -0300

    start the menu options

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,7 +6,11 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please, select a option bellow:");
+            Console.WriteLine("1 - Insert a new student");
+            Console.WriteLine("2 - List all students");
+            Console.WriteLine("3 - Calculate the score average");
+            Console.WriteLine();
             
         }
     }
