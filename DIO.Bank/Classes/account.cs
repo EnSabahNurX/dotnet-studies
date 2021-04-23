ec151a3e20f1edab80497a0c057d4bfe07173ca7
ricardo <ricklimadealmeida@gmail.com>
2021-04-23 12:38:16 -0300

    creating a method to check if the balance is enough

--- DIO.Bank/Classes/account.cs
+++ DIO.Bank/Classes/account.cs
@@ -1,6 +1,7 @@
 using System;
 namespace DIO.Bank
 {
+    // Attributes
     public class Account
     {
         private AccountType AccountType { get; set; }
@@ -8,11 +9,20 @@ namespace DIO.Bank
         private double Credit { get; set; }
         private string Name { get; set; }
     }
+    // Methods
     public Account(AccountType accountType, double balance, double credit, string name)
     {
         this.AccountType = accountType;
         this.Balance = balance;
         this.Credit = credit;
         this.Name = name;
+    }
+    public bool Withdraw(double withdrawAmount)
+    {
+        if (this.Balance - withdrawAmount < (this.Credit * -1))
+        {
+            Console.WriteLine("Insuficient Balance");
+            return false;
+        }
     }
 }
